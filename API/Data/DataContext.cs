f6933859029f98a2b0150d0f10f73c79d2f45be9
German <KostiakovG@Gmail.com>
2022-08-07 23:58:23 +0300

    created a context and initial migration

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<AppUser> Users { get; set; }
