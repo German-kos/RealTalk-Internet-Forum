7acee89b9905b0edb7d44b9294da2dd6fd32e622
German <KostiakovG@Gmail.com>
2022-08-09 11:56:27 +0300

    made an example for requesting data from api

--- Client/internet-forum/src/App.js
+++ Client/internet-forum/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [apiResponse, setApiResponse] = useState();
+  useEffect(() => {
+    var a = axios
+      .get("https://localhost:5001/api/users", { "Content-Type": "text/plain" })
+      .then((res) => setApiResponse(res.data) & console.log(res.data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API Request and Response Example</h1>
+      {apiResponse !== undefined
+        ? apiResponse?.map((user, i) => {
+            return (
+              <div key={i}>
+                {user.id}, {user.userName}, {user.password}
+              </div>
+            );
+          })
+        : "None"}
     </div>
   );
 }
