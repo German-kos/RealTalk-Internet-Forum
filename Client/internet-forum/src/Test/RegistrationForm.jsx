3eb02f2ac227b3541870952cd80b4ead37abe581
German <KostiakovG@Gmail.com>
2022-08-11 22:28:07 +0300

    added a working registration form for testing

--- Client/internet-forum/src/Test/RegistrationForm.jsx
+++ Client/internet-forum/src/Test/RegistrationForm.jsx
@@ -1,16 +1,42 @@
+import axios from "axios";
+import Button from "react-bootstrap/esm/Button";
 import Form from "react-bootstrap/Form";
 function RegistrationForm() {
+  var onRegisterClick = async (e) => {
+    e.preventDefault();
+
+    await axios({
+      method: "post",
+      url: "https://localhost:5001/api/account/register",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        username: e.target[0].value,
+        password: e.target[1].value,
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
-    <Form style={{ display: "flex", justifyContent: "center" }}>
-      <Form.Group>
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="username" placeholder="Enter Username" />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="text" placeholder="Enter Password" />
-      </Form.Group>
-    </Form>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Form onSubmit={(event) => onRegisterClick(event)} style={{}}>
+        <Form.Group className="mb3">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="username" placeholder="Enter Username" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="text" placeholder="Enter Password" />
+        </Form.Group>
+        <Button style={{ marginTop: "4px" }} variant="primary" type="submit">
+          Register User
+        </Button>
+      </Form>
+    </div>
   );
 }
 export default RegistrationForm;
