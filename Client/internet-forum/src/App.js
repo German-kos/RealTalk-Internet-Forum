301d805938f110ae0987c8621eb52e2a7360fd95
german-lpt <kostiakovg@gmail.com>
2022-08-16 23:09:04 +0300

    basic redux test demo

--- Client/internet-forum/src/App.js
+++ Client/internet-forum/src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
-import ReduxExample from "./Test/ReduxExample";
 import TestingPage from "./Test/TestingPage";
+import { useSelector, useDispatch } from "react-redux";
+//
+import { increment, decrement } from "./Redux/Actions";
+//
 function App() {
-  // return <TestingPage />;
-  return <ReduxExample/>
+	const counter = useSelector((state) => state.counter);
+	const isLogged = useSelector((state) => state.isLogged);
+	const dispatch = useDispatch();
+	//
+	return (
+		<div>
+			<h1>Counter: {counter}</h1>
+			<button onClick={() => dispatch(increment(2))}>+</button>
+			<button onClick={() => dispatch(decrement(5))}>-</button>
+			<h1>Is Logged: {isLogged.toString()}</h1>
+		</div>
+	);
 }
 
 export default App;
--- Client/internet-forum/src/index.js
+++ Client/internet-forum/src/index.js
@@ -5,40 +5,21 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 //
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import rootReducer from "./Redux/Reducers";
+import { Provider } from "react-redux";
 //
-const increment = () => {
-	return {
-		type: "INCREMENT",
-	};
-};
-const decrement = () => {
-	return {
-		type: "DECREMENT",
-	};
-};
-//
-const counter = (state = 0, action) => {
-	switch (action.type) {
-		case "INCREMENT":
-			return state + 1;
-		case "DECREMENT":
-			return state - 1;
-	}
-};
-//
-const store = configureStore({reducer:counter});
-//
-store.subscribe(()=> console.log(store.getState()))
-//
-store.dispatch(increment())
-store.dispatch(decrement())
-store.dispatch(decrement())
+const store = configureStore(
+	{ reducer: rootReducer },
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 //
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
 
