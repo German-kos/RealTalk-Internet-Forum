eaa7e5ef7b23ea5101ed6de0d4248b2e270f2534
GermanKos <kostiakovg@gmail.com>
2022-08-29 00:45:46 +0300

    refactored the registration and sign in process

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,48 +20,70 @@ namespace API.Controllers
             _context = context;
         }
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register([FromBody] UserEntity entity)
+        // Registration controller for the application
+        // As of right now, the registration controller expects to get these fields from the request:
+        // username, password, first name, last name, email. all fields are required
+        // the controller recieves and checks if the username is already taken.
+        // If the username already exists in the DB, a bad request is returned.
+        // If the username does not exist, it proceeds to the registration of the user and inserts him into the DB.
+        // The controller creates a new instance of the AppUser class, and adds the inserted data into it.
+        // For the password, it encrypts it using HMACSHA512, and instead of saving the password to the DB,
+        // it saves the hash and salt from the encryption.
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
-            if (UserAlreadyExists(entity.username))
+            if (await UserAlreadyExists(registerDTO.Username))
                 return BadRequest("This username is taken.");
-            byte[] passwordHash, passwordSalt;
-
-            using (var hmac = new HMACSHA512())
+            //
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
             {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(entity.password));
-            }
-            AppUser user = new AppUser();
-            user.UserName = entity.username;
-            user.FirstName = entity.firstName;
-            user.LastName = entity.lastName;
-            user.Email = entity.email;
-            user.PasswordHash = passwordHash;
-            user.PasswordSalt = passwordSalt;
+                UserName = registerDTO.Username,
+                FirstName = registerDTO.FirstName,
+                LastName = registerDTO.LastName,
+                Email = registerDTO.Email,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
             //
             _context.Users.Add(user);
             _context.SaveChanges();
             return user;
         }
         [HttpPost("signin")]
-        public async Task<ActionResult<SignedInUser>> SignIn([FromBody] LoginForm entity)
+        // Sign in controller for the application
+        // As of right now, the sign-in controller expects to get these fields from the requests:
+        // username, password. both fields are requires.
+        // The controller first looks for a user with the matching username in the DB,
+        // if it finds a user, it then uses the hashSalt from user found in the DB, and encrypts the password that was
+        // passed from the sign in request.
+        // It then compares the two encrypted passwords, the one from the DB user, and the second from the sign in request.
+        // If the hashes match, a DTO object is passed with the non-sensitive data related to the user.
+        // If the hashes do not match, a an http unauthorized error is returned instead.
+        public async Task<ActionResult<SignedInUserDTO>> SignIn(SignInDTO signInDTO)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(usr => usr.UserName.ToLower() == entity.username);
+            // get user from db
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName.ToLower() == signInDTO.Username.ToLower());
             if (user == null)
-                return null;
-            if (!MatchPasswordHash(entity.password, user.PasswordHash, user.PasswordSalt))
-                return null;
-            var userResult = new SignedInUser()
+                return Unauthorized("Invalid username or password");
+
+            // encrypt and compare passwords
+            if (PasswordsMatch(signInDTO.Password, user.PasswordHash, user.PasswordSalt))
             {
-                id = user.id,
-                username = user.UserName,
-                firstName = user.FirstName,
-                lastName = user.LastName,
-                email = user.Email
-            };
-            return userResult;
+                var signInUser = new SignedInUserDTO
+                {
+                    Id = user.id,
+                    Username = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.LastName
+                };
+                return signInUser;
+            }
+            else return Unauthorized("Invalid username or password");
         }
-        private bool MatchPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        // Pass the password from the sign-in request, password hash and salt from the user found in the DB
+        // The password from the request is then encrypted, and compared with the password hash from the DB
+        private bool PasswordsMatch(string password, byte[] passwordHash, byte[] passwordSalt)
         {
             using (var hmac = new HMACSHA512(passwordSalt))
             {
@@ -73,12 +96,12 @@ namespace API.Controllers
                 return true;
             }
         }
-        private bool UserAlreadyExists(string username)
+        // Pass a username (str) to check if the name is already in the DB.
+        // If the name is already in the DB, then the username is taken.
+        // T = in use, F = not in use
+        private async Task<bool> UserAlreadyExists(string username)
         {
-            var user = _context.Users.FirstOrDefault(usr => usr.UserName.ToLower() == username.ToLower());
-            if (user == null)
-                return false;
-            return true;
+            return await _context.Users.AnyAsync(user => user.UserName.ToLower() == username.ToLower());
         }
     };
 }
