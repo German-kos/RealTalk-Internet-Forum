19559cf967f59ecf632db06d08907e76e1ba323a
GermanKos <kostiakovg@gmail.com>
2022-09-02 23:00:27 +0300

    errors for sign up form

--- Client/internet-forum/src/components/signup-form/SignUpForm.tsx
+++ Client/internet-forum/src/components/signup-form/SignUpForm.tsx
@@ -14,33 +14,50 @@ export default function SignUpForm() {
     formState: { errors },
   } = useForm<SignUpInterface>();
   //
-
+  const submitSignUp = handleSubmit(async (data) => {
+    console.log(data);
+  });
   //
   const navToSignIn = () => navigate("/signin");
   return (
     <div className="sign-up_flex_container">
       <div className="sign-up_form_container">
-        <form className="sign-up_form">
+        <form className="sign-up_form" onSubmit={submitSignUp}>
           <div>
             <label>USERNAME</label>
             <input {...register("username", { required: true })} />
+            {errors.username && (
+              <div className="sign-up_form_error">This field is required</div>
+            )}
           </div>
           <div>
             <label>EMAIL</label>
-            <input {...register("username", { required: true })} />
+            <input {...register("email", { required: true })} />
+            {errors.email && (
+              <div className="sign-up_form_error">This field is required</div>
+            )}
           </div>
 
           <div>
             <label>PASSWORD</label>
             <input {...register("password", { required: true })} />
+            {errors.password && (
+              <div className="sign-up_form_error">This field is required</div>
+            )}
           </div>
           <div>
             <label>FIRST NAME</label>
             <input {...register("firstName", { required: true })} />
+            {errors.firstName && (
+              <div className="sign-up_form_error">This field is required</div>
+            )}
           </div>
           <div>
             <label>LAST NAME</label>
             <input {...register("lastName", { required: true })} />
+            {errors.lastName && (
+              <div className="sign-up_form_error">This field is required</div>
+            )}
           </div>
           <div>
             <button type="submit">SIGN UP</button>
