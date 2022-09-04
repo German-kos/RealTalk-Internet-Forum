25c74ba6b86e3f1b1a0e0a83951e99ea8217b543
GermanKos <kostiakovg@gmail.com>
2022-09-04 17:42:00 +0300

    remade sign up form

--- Client/internet-forum/src/utils/helpers.ts
+++ Client/internet-forum/src/utils/helpers.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
-import { ProcessResult, SignInInterface } from "./models";
+import {
+  AggragatedSignUpHooksInterface,
+  IProcessSignUpResult,
+  ProcessResult,
+  SignInInterface,
+  SignUpFormError,
+  SignUpInterface,
+} from "./models";
 import { validUsername } from "./regex";
 //
 
@@ -29,5 +36,46 @@ export const processSignInForm = (username: string, password: string) => {
     processResult.usernameIsOkay = true;
   if (processResult.resultMessagePassword === "")
     processResult.passwordIsOkay = true;
+  return processResult;
+};
+
+// a helper function to process the sign up form, and check for any errors in it's fields.
+export const processSignUpForm = (
+  target: SignUpInterface,
+  signUpHooks: AggragatedSignUpHooksInterface
+): IProcessSignUpResult => {
+  // the default value of processResult, will change if there are any errors in the sign up form
+  // to stop the sign up request, and ask the user to refill the form
+  const processResult: IProcessSignUpResult = { haltSignUp: false };
+  // a required field error for the error state hooks
+  const requiredFieldError: SignUpFormError = {
+    error: true,
+    errorMsg: "Required Field",
+  };
+  // tests for if the field are empty
+  if (target.username.value.trim().length === 0) {
+    processResult.haltSignUp = true;
+    signUpHooks.setUsernameError(requiredFieldError);
+  }
+
+  if (target.email.value.trim().length === 0) {
+    processResult.haltSignUp = true;
+    signUpHooks.setEmailError(requiredFieldError);
+  }
+
+  if (target.password.value.trim().length === 0) {
+    processResult.haltSignUp = true;
+    signUpHooks.setPasswordError(requiredFieldError);
+  }
+
+  if (target.firstName.value.trim().length === 0) {
+    processResult.haltSignUp = true;
+    signUpHooks.setFirstNameError(requiredFieldError);
+  }
+
+  if (target.lastName.value.trim().length === 0) {
+    processResult.haltSignUp = true;
+    signUpHooks.setLastNameError(requiredFieldError);
+  }
   return processResult;
 };
--- Client/internet-forum/src/utils/models.ts
+++ Client/internet-forum/src/utils/models.ts
@@ -6,13 +6,13 @@ export interface SignInInterface {
   password: string;
 }
 //
-export interface SignUpInterface {
-  username: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
+// export interface SignUpInterface {
+//   username: string;
+//   password: string;
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+// }
 //
 export interface ProcessResult {
   usernameIsOkay: boolean;
@@ -24,4 +24,29 @@ export interface ProcessResult {
 export interface FormErrorInterface {
   usernameError: string;
   passwordError: string;
+}
+// interface for error handler state in the sign up form component
+export interface SignUpFormError {
+  error: boolean;
+  errorMsg?: string;
+}
+// interface for the sign up form target.value
+export interface SignUpInterface {
+  username: { value: string };
+  password: { value: string };
+  email: { value: string };
+  firstName: { value: string };
+  lastName: { value: string };
+}
+// interface for a collection of hooks, for the error handlers in the sign up form
+export interface AggragatedSignUpHooksInterface {
+  setUsernameError: React.Dispatch<React.SetStateAction<SignUpFormError>>;
+  setEmailError: React.Dispatch<React.SetStateAction<SignUpFormError>>;
+  setPasswordError: React.Dispatch<React.SetStateAction<SignUpFormError>>;
+  setFirstNameError: React.Dispatch<React.SetStateAction<SignUpFormError>>;
+  setLastNameError: React.Dispatch<React.SetStateAction<SignUpFormError>>;
+}
+// process result interface for the processSignUpForm helper function
+export interface IProcessSignUpResult {
+  haltSignUp: boolean;
 }
